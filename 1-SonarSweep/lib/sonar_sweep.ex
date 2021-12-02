3402b51cda6c5fa3141c94701dabae9b5eade6a6
David Vrensk <david@vrensk.com>
2021-12-02 13:26:23 +0100

    Day 1! Got everything set up now.

---
+++ 1-SonarSweep/lib/sonar_sweep.ex
@@ -0,0 +1,34 @@
+defmodule SonarSweep do
+  @doc """
+  iex> SonarSweep.count_increases([1,5,6,4,7])
+  3
+  """
+  def count_increases([a, b | rest]) when a < b do
+    1 + count_increases([b | rest])
+  end
+
+  def count_increases([_, b | rest]) do
+    count_increases([b | rest])
+  end
+
+  def count_increases([_]) do
+    0
+  end
+
+  @doc """
+  iex> SonarSweep.count_in_windows([199,200,208,210,200,207,240,269,260,263])
+  5
+  """
+  def count_in_windows(list) do
+    collect_in_threes(list)
+    |> count_increases()
+  end
+
+  def collect_in_threes([a, b, c | rest]) do
+    [a+b+c | collect_in_threes([b,c | rest])]
+  end
+
+  def collect_in_threes(_) do
+    []
+  end
+end
---
+++ 1-SonarSweep/test/sonar_sweep_test.exs
@@ -0,0 +1,21 @@
+defmodule SonarSweepTest do
+  use ExUnit.Case
+  doctest SonarSweep
+
+  test "solves the first part" do
+    assert SonarSweep.count_increases((input_ints)) == 1475
+  end
+
+  test "solves the second part" do
+    assert SonarSweep.count_in_windows((input_ints)) == 1516
+  end
+
+  def input(path \\ "input.txt"), do: File.read!(path)
+  def input_lines(path \\ "input.txt"), do: input(path) |> String.split("\n", trim: true)
+  def input_paragraphs(path \\ "input.txt"), do: input(path) |> String.split("\n\n", trim: true)
+
+  def input_ints(path \\ "input.txt"),
+    do: input(path) |> String.split() |> Enum.map(&String.to_integer/1)
+
+  def assert_eq(a, b), do: assert(a == b)
+end
