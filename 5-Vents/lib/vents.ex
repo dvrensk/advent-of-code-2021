502f8c560415cb25ff5396fc6d811b4674e6336a
David Vrensk <david@vrensk.com>
2021-12-05 23:44:16 +0100

    Day 5

---
+++ 5-Vents/lib/vents.ex
@@ -0,0 +1,72 @@
+defmodule Vents do
+  @doc """
+  iex> Vents.sample()
+  ...> |> String.split("\\n", trim: true)
+  ...> |> Vents.count(2)
+  5
+  """
+  def count(list, minimum) do
+    list
+    |> Enum.map(fn s ->
+      Regex.scan(~r/\d+/, s)
+      |> List.flatten()
+      |> Enum.map(&String.to_integer/1)
+    end)
+    |> Enum.filter(fn [x1, y1, x2, y2] -> x1 == x2 || y1 == y2 end)
+    |> Enum.map(&expand/1)
+    |> List.flatten()
+    |> Enum.reduce(%{}, &Map.update(&2, &1, 1, fn a -> a + 1 end))
+    |> Enum.count(fn {_, c} -> c >= minimum end)
+  end
+
+  @doc """
+  iex> Vents.sample()
+  ...> |> String.split("\\n", trim: true)
+  ...> |> Vents.count2(2)
+  12
+  """
+  def count2(list, minimum) do
+    list
+    |> Enum.map(fn s ->
+      Regex.scan(~r/\d+/, s)
+      |> List.flatten()
+      |> Enum.map(&String.to_integer/1)
+    end)
+    |> Enum.map(&expand/1)
+    |> List.flatten()
+    |> Enum.reduce(%{}, &Map.update(&2, &1, 1, fn a -> a + 1 end))
+    |> Enum.count(fn {_, c} -> c >= minimum end)
+  end
+
+  def expand([x, y1, x, y2]) do
+    [y1, y2] = Enum.sort([y1, y2])
+    Enum.map(y1..y2, fn y -> {x, y} end)
+  end
+
+  def expand([x1, y, x2, y]) do
+    [x1, x2] = Enum.sort([x1, x2])
+    Enum.map(x1..x2, fn x -> {x, y} end)
+  end
+
+  def expand([x1, y1, x2, y2]) do
+    Enum.zip(
+      (if x1>x2, do: x1..x2//-1, else: x1..x2),
+      (if y1>y2, do: y1..y2//-1, else: y1..y2)
+    )
+  end
+
+  def sample() do
+    """
+    0,9 -> 5,9
+    8,0 -> 0,8
+    9,4 -> 3,4
+    2,2 -> 2,1
+    7,0 -> 7,4
+    6,4 -> 2,0
+    0,9 -> 2,9
+    3,4 -> 1,4
+    0,0 -> 8,8
+    5,5 -> 8,2
+    """
+  end
+end
---
+++ 5-Vents/test/vents_test.exs
@@ -0,0 +1,22 @@
+defmodule VentsTest do
+  use ExUnit.Case
+  doctest Vents
+
+  test "solves part 1" do
+    assert Vents.count(input_lines(), 2) == 8060
+  end
+
+  test "solves part 2" do
+    assert Vents.count2(input_lines(), 2) == 21577
+  end
+
+  def input(path \\ "input.txt"), do: File.read!(path)
+  def input_words(path \\ "input.txt"), do: input(path) |> String.split(~r/\s+/, trim: true)
+  def input_lines(path \\ "input.txt"), do: input(path) |> String.split("\n", trim: true)
+  def input_paragraphs(path \\ "input.txt"), do: input(path) |> String.split("\n\n", trim: true)
+
+  def input_ints(path \\ "input.txt"),
+    do: input(path) |> String.split() |> Enum.map(&String.to_integer/1)
+
+  def assert_eq(a, b), do: assert(a == b)
+end
