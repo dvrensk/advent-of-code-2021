86954011c76c1c52d5336438c38b842d8fc1bb71
David Vrensk <david@vrensk.com>
2021-12-06 15:40:29 +0100

    Day 6:2

--- 6-LanternFish/lib/lantern_fish.ex
+++ 6-LanternFish/lib/lantern_fish.ex
@@ -18,6 +18,36 @@ defmodule LanternFish do
     |> p1(n - 1)
   end
 
+  @doc """
+  iex> LanternFish.sample()
+  ...> |> LanternFish.p2(18)
+  26
+  iex> LanternFish.sample()
+  ...> |> LanternFish.p2(80)
+  5934
+  iex> LanternFish.sample()
+  ...> |> LanternFish.p2(256)
+  26984457539
+  """
+  def p2(list, n) when is_list(list) do
+    list
+    |> Enum.frequencies()
+    |> p2(n)
+  end
+
+  def p2(counts, 0), do: Map.values(counts) |> Enum.sum()
+
+  def p2(counts, n) do
+    0..8
+    |> Enum.map(fn
+      8 -> {8, Map.get(counts, 0, 0)}
+      6 -> {6, Map.get(counts, 0, 0) + Map.get(counts, 7, 0)}
+      n -> {n, Map.get(counts, n + 1, 0)}
+    end)
+    |> Map.new()
+    |> p2(n - 1)
+  end
+
   def sample() do
     [3, 4, 3, 1, 2]
   end
--- 6-LanternFish/test/lantern_fish_test.exs
+++ 6-LanternFish/test/lantern_fish_test.exs
@@ -6,6 +6,10 @@ defmodule LanternFishTest do
     assert LanternFish.p1(input_ints(), 80) == 390_923
   end
 
+  test "solves part 2" do
+    assert LanternFish.p2(input_ints(), 256) == 1_749_945_484_935
+  end
+
   def input(path \\ "input.txt"), do: File.read!(path)
   def input_words(path \\ "input.txt"), do: input(path) |> String.split(~r/\s+/, trim: true)
   def input_lines(path \\ "input.txt"), do: input(path) |> String.split("\n", trim: true)
